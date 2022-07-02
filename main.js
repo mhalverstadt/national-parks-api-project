12909da1dbeb14993a5eb249e134435b25802828
mhalverstadt <markhalverstadt@gmail.com>
2022-07-02 15:18:29 -0600

    updating main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('#title').autocomplete({
+        source: async function(request,response) {
+            let data= await fetch(`http://localhost:8000/search?query=${request.term}`)
+                    .then(results => results.json())
+                    .then(results => results.map(result => {
+                        return {
+                            label: result.title,
+                            value: result.title,
+                            id: result._id
+                        }
+                    }))
+                response(data)
+                //console.log(response)
+        },
+        minLength: 2,
+        select: function(event, ui) {
+            console.log(ui.item.id)
+            fetch(`http://localhost:8000/get/${ui.item.id}`)
+                .then(result => result.json())
+                .then(result => {
+                    $('#description').empty()
+                    result.description.forEach(description =>
+                        {
+                            $("#description").append(`<p>${description}</p>`)
+                        })
+                        // $('img').attr('src',result.poster)
+                })
+        }
+    })
+})
